e6613427e5dfd29589ccc393ce16963534c42e16
hamip <71255133+hamip@users.noreply.github.com>
2021-03-21 21:36:47 +0900

    Create 2490 윷놀이.py

---
+++ Baekjoon/2490 윷놀이.py
@@ -0,0 +1,8 @@
+a = list(map(int, input().split())).count(0)
+b = list(map(int, input().split())).count(0)
+c = list(map(int, input().split())).count(0)
+yut_List = ['E', 'A', 'B', 'C', 'D']
+
+print(yut_List[a])
+print(yut_List[b]) 
+print(yut_List[c])
