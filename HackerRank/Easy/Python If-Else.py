05ca43d086a43cecfba5202d6774b8822a33d9ae
hamip <71255133+hamip@users.noreply.github.com>
2021-03-28 21:32:56 +0900

    Create Python If-Else.py

---
+++ HackerRank/Easy/Python If-Else.py
@@ -0,0 +1,13 @@
+'''https://www.hackerrank.com/challenges/py-if-else/problem?h_r=next-challenge&h_v=zen'''
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n % 2 == 1:
+        print("Weird")
+    else:
+        if n >= 2 and n <= 5:
+            print("Not Weird")
+        elif n >= 6 and n <= 20:
+            print("Weird")
+        elif n > 20:
+            print("Not Weird")
