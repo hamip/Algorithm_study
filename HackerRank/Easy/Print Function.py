ec7b8e9bd9ef06b3c4e75fd5098821d019eb6b40
hamip <71255133+hamip@users.noreply.github.com>
2021-03-29 18:21:58 +0900

    Create Print Function.py

---
+++ HackerRank/Easy/Print Function.py
@@ -0,0 +1,8 @@
+'''[HackerRank] Python - Print Function'''
+
+'''Link:  www.hackerrank.com/challenges/python-print/problem'''
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i+1, end="")
