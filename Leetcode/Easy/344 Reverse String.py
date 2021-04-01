165a730634412cda0b4ecf288c44b63b09904098
hamip <71255133+hamip@users.noreply.github.com>
2021-04-01 21:01:06 +0900

    Create 344 Reverse String.py

---
+++ Leetcode/Easy/344 Reverse String.py
@@ -0,0 +1,12 @@
+'''https://leetcode.com/problems/reverse-string/'''
+
+'''#344 Reverse String'''
+
+'''"Life is short, use Python."'''
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
