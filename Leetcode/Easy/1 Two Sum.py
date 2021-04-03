d55651f9c61324c6f221fb974951b4760f8df8ff
hamip <71255133+hamip@users.noreply.github.com>
2021-04-03 15:29:48 +0900

    Create 1 Two Sum.py

---
+++ Leetcode/Easy/1 Two Sum.py
@@ -0,0 +1,13 @@
+'''https://leetcode.com/problems/two-sum/'''
+
+'''1. Two Sum'''
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_map = {}
+        
+        for i, num in enumerate(nums):
+            if target - num in nums_map:
+                return [nums_map[target-num], i]
+            nums_map[num] = i
+            
