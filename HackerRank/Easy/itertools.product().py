513e2d766a68a2132c7b48af3c3e4cf5859cbaae
hamip <71255133+hamip@users.noreply.github.com>
2021-04-07 13:47:47 +0900

    Create itertools.product().py

---
+++ HackerRank/Easy/itertools.product().py
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/itertools-product/problem
+
+# itertools.product()
+
+from itertools import product
+
+a = map(int, input().split())
+b = map(int, input().split())
+print(*product(a, b))
