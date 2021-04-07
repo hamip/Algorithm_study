f2edff2b554ebbc1272204a7f18cf10e1a07d083
hamip <71255133+hamip@users.noreply.github.com>
2021-04-07 13:27:41 +0900

    Create Integers Come In All Sizes.py

---
+++ HackerRank/Easy/Integers Come In All Sizes.py
@@ -0,0 +1,6 @@
+# https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+
+# Integers Come In All Sizes
+
+a, b, c, d = (int(input()) for _ in range(4))
+print(a ** b + c ** d)    # can use pow() method as well
