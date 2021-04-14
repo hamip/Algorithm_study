ac724cd2fd828a7cdd11269fde61a9fa2b0b6b2e
hamip <71255133+hamip@users.noreply.github.com>
2021-04-14 21:46:33 +0900

    Create 문자열 내 p와 y의 개수.py

---
+++ Programmers/level 1/문자열 내 p와 y의 개수.py
@@ -0,0 +1,9 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12916
+
+def solution(s: str) -> bool:
+    s = s.lower()
+    if s.count("p") == s.count("y"):
+        return True
+    else:
+        return False
+        
