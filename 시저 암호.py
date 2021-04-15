5e22ab9bd25730e65b04707f83cf6d42a0bd644f
hamip <71255133+hamip@users.noreply.github.com>
2021-04-15 18:16:06 +0900

    Create 시저 암호.py

---
+++ 시저 암호.py
@@ -0,0 +1,14 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12926
+
+def solution(s: str, n: int) -> str:
+    answer = ''
+    for letter in s:
+        if letter.isupper():
+            answer += chr((ord(letter) - ord('A') + n) % 26 + ord('A'))
+        elif letter.islower():
+            answer += chr((ord(letter) - ord('a') + n) % 26 + ord('a'))
+        else:
+            answer += letter
+    
+    return answer
+   
