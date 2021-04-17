8be72731f755a6c1bcc3c29cc9fe9af2d3d7a584
hamip <71255133+hamip@users.noreply.github.com>
2021-04-17 15:12:53 +0900

    Create 콜라츠 추측.py

---
+++ Programmers/level 1/콜라츠 추측.py
@@ -0,0 +1,15 @@
+# https://programmers.co.kr/learn/courses/30/lessons/12943
+
+def solution(num: int) -> int:
+    count = 0
+    while num != 1:
+        if count == 500:
+            return -1
+        
+        if num % 2 == 0:
+            num = num // 2
+        else:
+            num = num * 3 + 1
+        count += 1
+        
+    return count
