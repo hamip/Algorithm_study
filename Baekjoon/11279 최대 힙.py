0a53acc558e1ec9cc980b817ec787f82ddfc71c5
hamip <71255133+hamip@users.noreply.github.com>
2022-02-19 13:05:47 +0900

    Create 11279 최대 힙.py

---
+++ Baekjoon/11279 최대 힙.py
@@ -0,0 +1,14 @@
+import heapq, sys
+
+n = int(sys.stdin.readline())
+h = []
+
+for _ in range(n):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        if not h:
+            print(0)
+        else:
+            print(-heapq.heappop(h))
+    else:
+        heapq.heappush(h, -x)
