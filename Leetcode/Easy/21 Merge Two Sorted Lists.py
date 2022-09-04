3966f85eb4062e1b498ccaaed051ba2187d08ba1
hamip <71255133+hamip@users.noreply.github.com>
2022-09-05 03:25:54 +0900

    Update 21 Merge Two Sorted Lists.py

--- Leetcode/Easy/21 Merge Two Sorted Lists.py
+++ Leetcode/Easy/21 Merge Two Sorted Lists.py
@@ -1,22 +1,26 @@
-# https://leetcode.com/problems/merge-two-sorted-lists/
-
-# 21. Merge Two Sorted Lists
-
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        if (not l1) or (l2 and l1.val > l2.val):
-            l1, l2 = l2, l1
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        result = ListNode()
+        cur = result
+        
+        while list1 and list2:
+            if list1.val < list2.val:
+                cur.next = list1
+                list1 = list1.next
+            else:
+                cur.next = list2
+                list2 = list2.next
+                
+            cur = cur.next 
             
-        # recursive call
-        if l1:
-            l1.next = self.mergeTwoLists(l1.next, l2)
+        if list1:
+            cur.next = list1
+        if list2:
+            cur.next = list2
         
-        return l1
-        
-        
+        return result.next
